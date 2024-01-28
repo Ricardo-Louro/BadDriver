bf6fa584560404b0ba9d3a9005a9787e2665677f
Ricardo Almeida <a21807601@alunos.ulht.pt>
2024-01-28 15:44:33 +0000

    Updated scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private EnemySpawner spawner;
+    private Chaser chaser;
+    private Rigidbody rb;
+    [SerializeField] private int health;
+    [SerializeField] private int seconds;
+
+    private void Start()
+    {
+        chaser = gameObject.GetComponent<Chaser>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(health == 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(chaser);
+
+        rb.velocity = new Vector3();
+
+        //TRIGGER DEATH ANIMATION
+
+        spawner.QueueSpawn();
+        StartCoroutine(WaitToDestroy(seconds));
+    }
+
+    private IEnumerator WaitToDestroy(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Bullet"))
         {
-            Debug.Log("I GOT SHOT!");
+            health--;
         }
     }
 }
