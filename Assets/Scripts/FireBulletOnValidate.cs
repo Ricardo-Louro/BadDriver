4e3108215484e5b397656bd7e4e6c29d44142e74
Ricardo Louro <ricardo.f.louro@gmail.com>
2024-03-08 14:44:49 +0000

    Add animations and sounds. Fix various bugs. Remastered version ready

--- Assets/Scripts/FireBulletOnValidate.cs
+++ Assets/Scripts/FireBulletOnValidate.cs
@@ -5,6 +5,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class FireBulletOnValidate : MonoBehaviour
 {
+    private Transform                           playerTransform;
+    [SerializeField] private float              maxDistanceAllowed;
     [SerializeField] private GameObject         bullet;
     [SerializeField] private Transform          spawnPoint;
     [SerializeField] private float              bulletSpeed = 20f;
@@ -15,18 +17,29 @@ public class FireBulletOnValidate : MonoBehaviour
     [SerializeField] private float              maxAmmo;
     private float                               currentAmmo;
 
+    private ParticleSystem                      muzzleFlash;
+    private Animator                            animator;
+
+    [SerializeField] private AudioSource        emptyGunSound;
+    [SerializeField] private AudioSource        shootSound;
+
     // Start is called before the first frame update
     private void Start()
     {
+        playerTransform = FindObjectOfType<CarMoveForward>().transform;
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(FireBullet);
         currentAmmo = maxAmmo;
+        muzzleFlash = GetComponentInChildren<ParticleSystem>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        
+        if ((transform.position - playerTransform.position).magnitude > maxDistanceAllowed)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void FireBullet(ActivateEventArgs arg)
@@ -39,6 +52,13 @@ public class FireBulletOnValidate : MonoBehaviour
             spawnedBullet.transform.position = spawnPoint.position;
             spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * bulletSpeed;
             Destroy(spawnedBullet, 5);
+            muzzleFlash.Play();
+            shootSound.Play();
+            animator.SetTrigger("shooting");
+        }
+        else if(currentAmmo <= 0)
+        {
+            emptyGunSound.Play();
         }
     }
 }
